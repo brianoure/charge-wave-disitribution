4abc249e0420db428a645d5730eb2215d8073460
brianoure <61065227+brianoure@users.noreply.github.com>
2022-09-22 22:16:55 +0300

    Update charge_wave_distribution.py

--- charge_wave_distribution.py
+++ charge_wave_distribution.py
@@ -16,17 +16,19 @@ def mag(x,y,z):
   
 class POINT3D:
   def __init__(this,x,y,z,sph_radius=0,sph_elevation_rad=0,sph_azimuth_rad=0,cyl_radius=0,cyl_height=0,cyl_azimuth_rad=0):
+    if( ((sph_radius*sph_elevation_rad*sph_azimuth_rad)+(cyl_radius*cyl_height*cyl_azimuth_rad)) ==0  ):
+      this.x = x
+      this.y = y
+      this.z = z
+      this.magnitude     = mag(x,y,z)
+      this.sphere_radius = this.magnitude
+      this.sphere_elevation_rad = math.acos( ( mag(x,y,0) )/(this.magnitude) )
+      this.sphere_azimuth_rad   = math.atan( z/x )
+      this.cylinder_radius = mag(x,0,z)
+      this.cylinder_height = y
+      this.cylinder_azimuth_rad = this.sphere_azimuth_rad
     if( sph_radius*sph_elevation_rad*sph_azimuth_rad ):
       pass
     if( cyl_radius*cyl_height*cyl_azimuth_rad        ):
       pass
-    this.x = x
-    this.y = y
-    this.z = z
-    this.magnitude     = mag(x,y,z)
-    this.sphere_radius = this.magnitude
-    this.sphere_elevation_rad = math.acos( ( mag(x,y,0) )/(this.magnitude) )
-    this.sphere_azimuth_rad   = math.atan( z/x )
-    this.cylinder_radius = mag(x,0,z)
-    this.cylinder_height = y
-    this.cylinder_azimuth_rad = this.sphere_azimuth_rad
+    
